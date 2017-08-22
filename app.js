60f7f24b7973de65b029f67a3e241787bd926b1e
charliematina <charliematina@gmail.com>
2017-08-23 11:55:45 +1200

    First Push

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var cors = require("cors");
+var config = require("./config");
+var path = require("path");
+
+var app = express();
+
+app.use(cors());
+app.use(express.static("./public"));
+
+// jquery
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
+
+app.use(function(request, response, next){
+	console.log(`${request.method} request for ${request.url}`); 
+	next();
+})
+
+
+
+
+app.listen(3000);
+
+console.log("Server running on port 3000");
