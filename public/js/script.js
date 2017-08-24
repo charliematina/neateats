b97b94be38d088c893869daa1f95e74719843584
charliematina <charliematina@gmail.com>
2017-08-25 00:04:31 +1200

    added facebook api data. Changed student json file adding in ids for facebook pages

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,121 @@
 // Global Variables
 var map;
+var fbToken = "EAACEdEose0cBAIQyUuYsBgByFCZAEN0NZAUnE7ZAN1Qc8Nc75E3VdifqQIJZCAj3O2ogJw7e5vZB9IzvZC15Nzd2fNg168RXNOJ5wpAQJKEr27d652zjIBJXzF8W7kaeaofHl4omflPRUFPfwCOvU0JgRE0NuRe69K6DzzeLAeuRF5NhZA1QvTsEZBZCZAu1zlgKdcMnoacQVxlgZDZD";
+var pageId;
+var placeName;
+var placeAbout;
+var placeStreet;
+var placeCity;
+var placeImage;
+var lat;
+var long;
+var price;
+var placePhone;
+var placeWebsite;
+var placePrice;
+var studentName;
+var studentData;
+var choiceId1;
+var choiceId2;
+var choiceId3;
+var choice1;
+var choice2;
+var choice3;
+var gender;
 
 // Calling Functions
 initialiseMap();
+getStudentData();
 
-$.ajax({
+function getStudentData(){
+	$.ajax({
        url: "http://localhost:3000/students",
        contentType: "application/json",
        dataType: "json",
        success: function(data){
        	for (var i = 0; i < data.length; i++) {
-       		var studentName = data[i].name;
-
-       		$("#nameList").append("<li class='listName'><div class='navHoverBlock'></div>"+studentName+"</li>")
+       		studentData = data;
+       		studentName = data[i].name;
+   			studentName = data[i].name;
+       		choiceId1 = data[i].choice1.id;
+       		choiceId2 = data[i].choice2.id;
+       		choiceId3 = data[i].choice3.id;
+       		choice1 = data[i].choice1.name;
+       		choice2 = data[i].choice2.name;
+       		choice3 = data[i].choice3.name;
+       		$("#nameList").append("<li class='listName navName'><div class='navHoverBlock'></div><span class='navNameContainer'>"+studentName+"<span></li>");
        	}
        },
        error: function(){
             console.log("Error");
        }
-});
+	});
+}
 
-
-
-
-$(".listName").hover(function(){
-		if($(this).hasClass("clicked")){
+$(document).on('mouseover', '.navName', function(){
+	if($(this).hasClass("clicked")){
 		} else{
 			$(this).find(".navHoverBlock").css("width","15px");
 		}
-	}, function(){
-			if($(this).hasClass("clicked")){
+}).on('mouseout', '.navName', function(){
+	if($(this).hasClass("clicked")){
 		} else{
 			$(this).find(".navHoverBlock").css("width","0px");
 		}
-}).click(function(){
-	$(".listName").removeClass('clicked').css("color","white").find(".navHoverBlock").css("width","0%");
+}).on('click', '.navName', function(){
+	studentName = $(this).text();
+	for (var i = 0; i < studentData.length; i++) {
+		if(studentData[i].name == $(this).text()){
+			choiceId1 = studentData[i].choice1.id;
+       		choiceId2 = studentData[i].choice2.id;
+       		choiceId3 = studentData[i].choice3.id;
+       		choice1 = studentData[i].choice1.name;
+       		choice2 = studentData[i].choice2.name;
+       		choice3 = studentData[i].choice3.name;
+       		if(studentData[i].gender == "female"){
+       			gender = "img/female.png";
+       		} else{
+       			gender = "img/male.png";	
+       		}
+		}
+	};
+	$(".navName").removeClass('clicked').css("color","white").find(".navHoverBlock").css("width","0%");
+	// $(this).addClass('clicked').css("color","#db2940").find(".navHoverBlock").css("width","100%");
+	$("#goBackButton").fadeIn(400);
+	$("#studentChoices").fadeIn(400);
+	$("#studentNames").hide();
+	$("#backToTop").hide();
+	$("#studentImage").attr('src', gender);
+	$("#placesList").empty().append(
+		"<li class='listName studentChoice' data-type='"+choiceId1+"'><div class='navHoverBlock'></div><span class='navNameContainer'>"+choice1+"<span></li>"
+		+"<li class='listName studentChoice' data-type='"+choiceId2+"'><div class='navHoverBlock'></div><span class='navNameContainer'>"+choice2+"<span></li>"
+		+"<li class='listName studentChoice' data-type='"+choiceId3+"'><div class='navHoverBlock'></div><span class='navNameContainer'>"+choice3+"<span></li>"
+		);
+	$("#studentName").empty().append("<h2>"+studentName+"'s TOP 3</h2>");
+});
+
+$(document).on('mouseover', '.studentChoice', function(){
+	if($(this).hasClass("clicked")){
+		} else{
+			$(this).find(".navHoverBlock").css("width","15px");
+		}
+}).on('mouseout', '.studentChoice', function(){
+	if($(this).hasClass("clicked")){
+		} else{
+			$(this).find(".navHoverBlock").css("width","0px");
+		}
+}).on('click', '.studentChoice', function(){
+	placeId = $(this).attr('data-type');
+	getFbData(placeId);
+	$(".studentChoice").removeClass('clicked').css("color","white").find(".navHoverBlock").css("width","0%");
 	$(this).addClass('clicked').css("color","#db2940").find(".navHoverBlock").css("width","100%");
-	$("#studentNames").fadeOut();
-	$("#studentChoices").fadeIn();
 	openOverlay();
-})
+});
 
 $("#closeButton").click(function(){
 	closeOverlay();
 })
+
 
 function openOverlay(){
 	$("#detailsOverlay").css({"height":"100%","padding-top":"80px"});
@@ -51,18 +123,76 @@ function openOverlay(){
 
 function closeOverlay(){
 	$("#detailsOverlay").css({"height":"0%","padding-top":"0px"});
+	$(".studentChoice").removeClass('clicked').css("color","white").find(".navHoverBlock").css("width","0%");
 }
 
-$("#backToTop").hover(function(){
+function buttonHover(buttonId){
+	$(buttonId).hover(function(){
 		$(this).css("color","#db2940").find(".buttonHoverBlock").css("width","100%");
 	}, function(){
 		$(this).css("color","white").find(".buttonHoverBlock").css("width","0%");
-})
+	})
+}
+
+buttonHover("#backToTop");
+buttonHover("#goBackButton");
 
 $("#backToTop").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
+
+$("#goBackButton").hide().click(function() {
+  	closeOverlay();
+  	$(this).hide();
+  	$("#studentChoices").hide();
+	$("#studentNames").fadeIn(500);
+  	$("#backToTop").fadeIn(500);
+  	$("#details").delay(500).queue(function(next){
+  		$(this).empty();
+  	});
+  	$(this).removeClass('clicked').css("color","white").find(".navHoverBlock").css("width","0%"); 
+});
+
+function getFbData(locationId){
+	$.ajax({
+       url: "https://graph.facebook.com/v2.10/"+locationId+"?fields=name%2Cabout%2Cprice_range%2Clocation%2Cid%2Coverall_star_rating%2Chours%2Cpicture%7Burl%7D%2Cphone%2Cwebsite&access_token="+fbToken,
+       contentType: "application/json",
+       dataType: "jsonp",
+       success: function(fbData){
+       	console.log(fbData);
+			placeName = fbData.name;
+			placeCity = fbData.location.city;
+			placeStreet = fbData.location.street;
+			placeLat = fbData.location.latitude;
+			placeLong = fbData.location.longitude;
+			placeRating = fbData.overall_star_rating;
+			placeHours = fbData.hours;
+			placeImage = fbData.picture.data.url;
+			placeAbout = fbData.about;
+			placePhone = fbData.phone;
+			placeWebsite = fbData.website;
+			placePrice = fbData.price_range;
+			$("#details").empty().append(
+				// "<div id='closeButtonContainer'><img id='closeButton' src='img/cross.png'></div>"+
+				"<div id='placeImage'><img src='"+placeImage+"'></div>"+
+				"<div id='placeName'>"+placeName+"</div>"+
+				"<div id='placeAbout'>"+placeAbout+"</div>"+
+				"<div id='placeAddress'>"+placeStreet+"<br>"+placeCity+"</div>"+
+				"<div id='placePhone'>PHONE<br>"+placePhone+"</div>"+
+				"<div id='placeRating'><div class='detailLabel'>RATING</div>"+placeRating+"</div>"+
+				"<div id='placeWebsite'><a target='_blank' href='"+placeWebsite+"'>"+placeWebsite+"</a></div>"
+			);
+			if(placeName.length > 15 ){
+				// $("#placeName").css('font-size','2em');
+				$("#placeName").addClass('largeName');
+			};
+       },
+       error: function(){
+            console.log("Error");
+       }
+	});
+}
 
 function initialiseMap(){
 	// Setting the position of the map as well as the default interactivity
